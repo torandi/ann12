073aeb923a8b17d86a960836d648168580c0a4d9
Andreas Tarandi <torandi@gmail.com>
2012-02-14 18:30:55 +0100

    Parts of lab2

---
+++ lab2/four_one_square2.m
@@ -0,0 +1,17 @@
+
+% Input:
+% units = number of units
+
+x = [0:0.1:2*pi]';
+f = square(2*x);
+
+makerbf
+
+phi = calcPhi(x, m, var);
+
+w = phi\f;
+
+y = phi*w;
+
+rbfplot1(x, y, f, units); 
+% subplot(2, 1, 1); plot(x, phi);
